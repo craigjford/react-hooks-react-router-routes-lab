2ad111c4bd642453dc1ca79a4a40737681749eb6
Craig Ford <craig_ford@icloud.com>
2022-06-14 21:36:39 -0400

    finished App and NavBar

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const moviesDisplay = movies.map((movie) => {
+   return (
+      <div>
+        <p>{movie.title}</p>
+        <p>{movie.time}</p>
+        <ul>
+          {movie.genres.map(movie.genre => <li>{movie.genre}</li>)}
+        </ul>
+      </div>  
+   )
+  })
+
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      {moviesDisplay}
+    </div>
+  );  
 }
 
 export default Movies;
